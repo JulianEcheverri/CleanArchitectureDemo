baf898b94d04b60a58043123e0f0dc6d2bed643e
Julián Echeverri <julianecheverri@outlook.com>
2021-11-17 11:17:43 -0500

    Adding Infrastructure Data layer and UniversityDbContext in order to create table Courses

---
+++ CleanArchitectureDemo/CleanArchitectureDemo.Infrastructure.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureDemo.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitectureDemo.Infrastructure.Data.Context
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
