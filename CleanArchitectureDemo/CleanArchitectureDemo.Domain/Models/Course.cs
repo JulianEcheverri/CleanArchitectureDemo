ffe6dc9453c7033b6f208dca06248d4123a283c3
Julián Echeverri <julianecheverri@outlook.com>
2021-11-17 11:05:32 -0500

    Setting up domain layer and adding Couse model within

---
+++ CleanArchitectureDemo/CleanArchitectureDemo.Domain/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitectureDemo.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
