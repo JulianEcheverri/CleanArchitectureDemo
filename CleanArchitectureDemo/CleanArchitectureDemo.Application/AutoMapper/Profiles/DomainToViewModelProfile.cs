be91c4576f6abc8f3090efde24248c12933fc81c
Julián Echeverri <julianecheverri@outlook.com>
2021-11-19 16:07:54 -0500

    Profiles definition for AutoMapper approach

---
+++ CleanArchitectureDemo/CleanArchitectureDemo.Application/AutoMapper/Profiles/DomainToViewModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CleanArchitectureDemo.Application.ViewModels;
+using CleanArchitectureDemo.Domain.Models;
+
+namespace CleanArchitectureDemo.Application.AutoMapper.Profiles
+{
+    public class DomainToViewModelProfile : Profile
+    {
+        public DomainToViewModelProfile()
+        {
+            CreateMap<Course, CourseViewModel>();
+        }
+    }
+}
---
+++ CleanArchitectureDemo/CleanArchitectureDemo.Application/AutoMapper/Profiles/ViewModelToDomainProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchitectureDemo.Application.ViewModels;
+using CleanArchitectureDemo.Domain.Commands;
+
+namespace CleanArchitectureDemo.Application.AutoMapper.Profiles
+{
+    public class ViewModelToDomainProfile : Profile
+    {
+        public ViewModelToDomainProfile()
+        {
+            CreateMap<CourseViewModel, CreateCourseCommand>()
+                .ConstructUsing(x => new CreateCourseCommand(x.Name, x.Description, x.ImageUrl));
+        }
+    }
+}
